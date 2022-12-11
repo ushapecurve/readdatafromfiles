41297ec172f6cd2836ff049a10a50c4e701c3451
romanlukichev <roman.lukichev@yahoo.com>
2022-12-10 17:54:18 -0800

    Renamed variables. Saved graphs into .png images

--- main.py
+++ main.py
@@ -10,65 +10,70 @@ import matplotlib.pyplot as plt1
 import matplotlib.pyplot as plt2
 
 
-TxtList1 = readtextfile()      # made list datastructure (linked list)
-DbList1 = readdatabase()  # made list datastructure (linked list)
+txtList1 = readtextfile()      # made list datastructure (linked list)
+dbList1 = readdatabase()  # made list datastructure (linked list)
 
-TxtSeries = pd.Series(TxtList1)  # made a series from CSVlist using pandas library
-DbSeries = pd.Series(DbList1) # made a series from DBlist using pandas library
+txtSeries = pd.Series(txtList1)  # made a series from CSVlist using pandas library
+dbSeries = pd.Series(dbList1) # made a series from DBlist using pandas library
 # series is pandas library datastructure. 
-# We can print this seies using print(CSVseries) print(DBseries)
+# We can print this seies using print(CSVseries) print(dbSeries)
 
 print('Data from csv file:')
-print(TxtList1)
+print(txtList1)
 
 print('Data from database file')
-print(DbList1)
+print(dbList1)
 
 # Now we make bar diagram using matplotlib library
-TxtIndexes = TxtSeries.index   # separated indexes from CSVseries
-TxtValues = TxtSeries.values   # separated values from CSVseries
+txtIndexes = txtSeries.index   # separated indexes from CSVseries
+txtValues = txtSeries.values   # separated values from CSVseries
 # print('Type of csv indexes array', type(CSVindexes))
-Xvalues = np.array(TxtIndexes)  # created numpy array from indexes 
-Yvalues = np.array(TxtValues)  # created numpy array from values
+Xvalues = np.array(txtIndexes)  # created numpy array from indexes 
+Yvalues = np.array(txtValues)  # created numpy array from values
 # values will be displayed on verical axis
 plt1.bar(Xvalues, Yvalues, color = 'red')  # made bar diagram using matplotlib library
 plt1.title('Temperatures read from TXT file')
-plt1.ylabel('Temperatures')
-plt1.xlabel('No of measurement')
+plt1.ylabel('Temperatures, F')
+plt1.xlabel('Number of measurement')
+plt1.savefig('txtFileChart.png', dpi=600)
 plt1.show()  # showed the bar diagram
+
 
 # print(csvList1)
 # print(dbList1) 
 
 # Now we make bar diagram using matplotlib library
-DbIndexes = DbSeries.index   # separated indexes from CSVseries
-DbValues = DbSeries.values   # separated values from CSVseries
+dbIndexes = dbSeries.index   # separated indexes from CSVseries
+dbValues = dbSeries.values   # separated values from CSVseries
 # print('Type of csv indexes array', type(CSVindexes))
-Xvalues = np.array(DbIndexes)  # created numpy array from indexes 
-Yvalues = np.array(DbValues)  # created numpy array from values
+Xvalues = np.array(dbIndexes)  # created numpy array from indexes 
+Yvalues = np.array(dbValues)  # created numpy array from values
 # values will be displayed on verical axis
 plt2.bar(Xvalues, Yvalues, color = 'green')  # made bar diagram using matplotlib library
 plt2.title('Temperatures read from Database file')
-plt2.ylabel('Temperatures')
-plt2.xlabel('No of measurement')
+plt2.ylabel('Temperatures, F')
+plt2.xlabel('Number of measurement')
+plt2.savefig('dbFileChart.png', dpi=600)
 plt2.show()  # showed the bar diagram
 
 # We will show descriptive statistics of CSVseries
 # Now we make dataframe from CSVseries.  
 # dataframe is pandas library datastructure
-DfTXT = pd.DataFrame(TxtSeries, columns=['Data'])
+dfTXT = pd.DataFrame(txtSeries, columns=['Data'])
 print('Dataframe from TXT file')
-print(DfTXT)
-print('Dataframe has descriptive statistics:')
-print(DfTXT.describe())
+print(dfTXT)
+print('Dataframe from TXT file')
+print('has descriptive statistics:')
+print(dfTXT.describe())
 
-# We will show descriptive statistics of DBseries
-# Now we make dataframe from DBseries
+# We will show descriptive statistics of dbSeries
+# Now we make dataframe from dbSeries
 # Now we make dataframe.  # dataframe is pandas library datastructure
-dfDB = pd.DataFrame(DbSeries, columns=['Temperature'])
+dfDB = pd.DataFrame(dbSeries, columns=['Temperature'])
 print('Dataframe from DATBASE file')
 print(dfDB)
-print('Dataframe has descriptive statistics:')
+print('Dataframe from DATBASE file')
+print('has descriptive statistics:')
 print(dfDB.describe())
 
 
--- printdataframeinfo.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-    
-def printDFInfo(pandas.DataFrame, descriptionString)
-    print(descriptionString)
-    print(DataFrame)
-    print('Has descriptive statistics:')
-    print(dfDB.describe())
