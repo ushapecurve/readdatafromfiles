1b3a87d578621af38f8eccf6740e6239ffd8d607
romanlukichev <roman.lukichev@yahoo.com>
2022-12-13 22:15:06 -0800

    Final stage of the progect before presetation at Laney College in 12/14/2022 at 1 p.m.

--- main.py
+++ main.py
@@ -13,25 +13,29 @@ import matplotlib.pyplot as plt2
 txtList1 = readtextfile()      # made list datastructure (linked list)
 dbList1 = readdatabase()  # made list datastructure (linked list)
 
-txtSeries = pd.Series(txtList1)  # made a series from CSVlist using pandas library
-dbSeries = pd.Series(dbList1) # made a series from DBlist using pandas library
+txtPsandasSeries = pd.Series(txtList1)  # made a series object from TXTlist using pandas library
+dbPandasSeries = pd.Series(dbList1) # made a series object from DBlist using pandas library
 # series is pandas library datastructure. 
-# We can print this seies using print(CSVseries) print(dbSeries)
+# We can print this seies using print(CSVseries) print(dbPandasSeries)
 
-print('Data from csv file:')
+print('Data from text file:')
 print(txtList1)
+#print('Pandas series datastructure made from TXT file')
+#print(txtPsandasSeries)  # series is just one dimentional array that can hold data of any type.
 
 print('Data from database file')
 print(dbList1)
+#print('Pandas series datastructure made from TXT file')
+#print(dbPandasSeries)  # series is just one dimentional array that can hold data of any type.
 
 # Now we make bar diagram using matplotlib library
-txtIndexes = txtSeries.index   # separated indexes from CSVseries
-txtValues = txtSeries.values   # separated values from CSVseries
+txtIndexes = txtPsandasSeries.index   # separated indexes from CSVseries
+txtValues = txtPsandasSeries.values   # separated values from CSVseries
 # print('Type of csv indexes array', type(CSVindexes))
-Xvalues = np.array(txtIndexes)  # created numpy array from indexes 
-Yvalues = np.array(txtValues)  # created numpy array from values
+xValues = np.array(txtIndexes)  # created numpy array from indexes 
+yValues = np.array(txtValues)  # created numpy array from values
 # values will be displayed on verical axis
-plt1.bar(Xvalues, Yvalues, color = 'red')  # made bar diagram using matplotlib library
+plt1.bar(xValues, yValues, color = 'red')  # made bar diagram using matplotlib library
 plt1.title('Temperatures read from TXT file')
 plt1.ylabel('Temperatures, F')
 plt1.xlabel('Number of measurement')
@@ -43,13 +47,13 @@ plt1.show()  # showed the bar diagram
 # print(dbList1) 
 
 # Now we make bar diagram using matplotlib library
-dbIndexes = dbSeries.index   # separated indexes from CSVseries
-dbValues = dbSeries.values   # separated values from CSVseries
+dbIndexes = dbPandasSeries.index   # separated indexes from CSVseries
+dbValues = dbPandasSeries.values   # separated values from CSVseries
 # print('Type of csv indexes array', type(CSVindexes))
-Xvalues = np.array(dbIndexes)  # created numpy array from indexes 
-Yvalues = np.array(dbValues)  # created numpy array from values
+xValues = np.array(dbIndexes)  # created numpy array from indexes 
+yValues = np.array(dbValues)  # created numpy array from values
 # values will be displayed on verical axis
-plt2.bar(Xvalues, Yvalues, color = 'green')  # made bar diagram using matplotlib library
+plt2.bar(xValues, yValues, color = 'green')  # made bar diagram using matplotlib library
 plt2.title('Temperatures read from Database file')
 plt2.ylabel('Temperatures, F')
 plt2.xlabel('Number of measurement')
@@ -59,19 +63,19 @@ plt2.show()  # showed the bar diagram
 # We will show descriptive statistics of CSVseries
 # Now we make dataframe from CSVseries.  
 # dataframe is pandas library datastructure
-dfTXT = pd.DataFrame(txtSeries, columns=['Data'])
-print('Dataframe from TXT file')
-print(dfTXT)
+dfTXT = pd.DataFrame(txtPsandasSeries, columns=['Data'])
+#print('Dataframe from TXT file')
+#print(dfTXT)
 print('Dataframe from TXT file')
 print('has descriptive statistics:')
 print(dfTXT.describe())
 
-# We will show descriptive statistics of dbSeries
-# Now we make dataframe from dbSeries
+# We will show descriptive statistics of dbPandasSeries
+# Now we make dataframe from dbPandasSeries
 # Now we make dataframe.  # dataframe is pandas library datastructure
-dfDB = pd.DataFrame(dbSeries, columns=['Temperature'])
-print('Dataframe from DATBASE file')
-print(dfDB)
+dfDB = pd.DataFrame(dbPandasSeries, columns=['Temperature'])
+#print('Dataframe from DATBASE file')
+#print(dfDB)
 print('Dataframe from DATBASE file')
 print('has descriptive statistics:')
 print(dfDB.describe())
